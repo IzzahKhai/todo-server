f3620590086f92d20b1c7e99afcec3d6a7a491ea
Izzah Khairina Muhadi <izzkhai96@gmail.com>
2022-06-08 10:33:44 +0800

    :wrench: Remove google authentication sign in.

--- src/main/java/com/example/todoserver/security/WebSecurityConfig.java
+++ src/main/java/com/example/todoserver/security/WebSecurityConfig.java
@@ -1,21 +1,21 @@
-package com.example.todoserver.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .antMatcher("/**").authorizeRequests()
-                .antMatchers("/task").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
-    }
-}
+//package com.example.todoserver.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http
+//                .antMatcher("/**").authorizeRequests()
+//                .antMatchers("/task").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .oauth2Login();
+//    }
+//}
